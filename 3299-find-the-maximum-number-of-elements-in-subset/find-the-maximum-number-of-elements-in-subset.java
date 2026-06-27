230e0d89a5be015341e1ebe07b12b46c98d47103
Tulasi-Andimeni <24M11MC002@adityauniversity.in>
2026-06-27 12:13:13 +0530

    Time: 54 ms (72.09%) | Memory: 71.8 MB (9.30%) - LeetSync

---
+++ 3299-find-the-maximum-number-of-elements-in-subset/find-the-maximum-number-of-elements-in-subset.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int maximumLength(int[] nums) {
+        Map<Long , Integer> freq = new HashMap<>();
+
+        for(int x : nums){
+            freq.put((long)x, freq.getOrDefault((long)x, 0)+1);
+        }
+        int ans = 1;
+        if(freq.containsKey(1L)){
+            int cnt = freq.get(1L);
+            ans = Math.max(ans, cnt %2 == 0 ? cnt - 1: cnt);
+        }
+
+        for(long x : freq.keySet()){
+            if(x == 1)
+                continue;
+            
+            long cur = x;
+            int len = 0;
+
+            while(true){
+                int cnt = freq.getOrDefault(cur, 0);
+
+                if(cnt >= 2){
+                    len += 2;
+                    cur = cur * cur;
+
+                    if(cur > 1_000_000_000_000_000_000L)
+                        break;
+                    
+                }else if(cnt == 1){
+                    len++;
+                    break;
+                }else{
+                    len--;
+                    break;
+                }
+                
+
+                
+                
+            }
+            ans = Math.max(ans, len);
+        }
+        return ans;
+        
+    }
+}
